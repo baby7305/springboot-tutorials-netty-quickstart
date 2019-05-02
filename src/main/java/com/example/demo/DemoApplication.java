cac3aae0df45006530b175535385e83457398ad7
baby7305 <songhaibei7305@163.com>
2019-05-02 21:29:10 +0800

    socket简单范例，多个客户端会被第一个阻塞卡住

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,59 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(DemoApplication.class, args);
+
+		//创建socket服务,监听10101端口
+		ServerSocket server = new ServerSocket(10101);
+		System.out.println("服务器启动！");
+		while (true) {
+			//获取一个套接字（阻塞）
+			final Socket socket = server.accept();
+			System.out.println("来个一个新客户端！");
+			//业务处理
+			handler(socket);
+		}
+	}
+
+	/**
+	 * 读取数据
+	 *
+	 * @param socket
+	 * @throws Exception
+	 */
+	public static void handler(Socket socket) {
+		try {
+			byte[] bytes = new byte[1024];
+			InputStream inputStream = socket.getInputStream();
+
+			while (true) {
+				//读取数据（阻塞）
+				int read = inputStream.read(bytes);
+				if (read != -1) {
+					System.out.println(new String(bytes, 0, read));
+				} else {
+					break;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				System.out.println("socket关闭");
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
