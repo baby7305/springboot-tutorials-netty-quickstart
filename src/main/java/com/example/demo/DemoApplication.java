4f67ff71e83c6b6917b1f24a4ddb20d8de51cc29
baby7305 <songhaibei7305@163.com>
2019-05-20 23:48:56 +0800

    netty基于nio，抽取源码，引入线程池

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import com.example.demo.config.ServerBootstrap;
+import com.example.demo.pool.NioSelectorRunnablePool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		//初始化线程
+		NioSelectorRunnablePool nioSelectorRunnablePool = new NioSelectorRunnablePool(Executors.newCachedThreadPool(), Executors.newCachedThreadPool());
+
+		//获取服务类
+		ServerBootstrap bootstrap = new ServerBootstrap(nioSelectorRunnablePool);
+
+		//绑定端口
+		bootstrap.bind(new InetSocketAddress(10101));
+
+		System.out.println("start");
 	}
 
 }
